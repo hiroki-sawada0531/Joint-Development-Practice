f9ac8085ce8c5b783fbdef822d7af256313ae82b
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-09 01:51:37 +0900

    addとcreateアクションの追加

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,23 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index() {
+    public function index(Request $request)
+    {
         return view('profile.index');
+    }
+
+    public function add(Request $request)
+    {
+        return view('profile.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+        unset($form['_token']);
+        $profile->fill($form)->save();
+        return redirect('/profile');
     }
 }
