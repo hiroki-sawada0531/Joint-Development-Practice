2f258dbd9099fed58c72c3bff5ceed45e1907104
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-09 23:05:34 +0900

    layout作成

---
+++ resources/views/Profile/add.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.header')
+@push('css')
+  <link rel="stylesheet" href="{{asset('/css/index.css')}}">
+@endpush
+
+@section('content')
+    <div class="main">
+        <form action="" method="post">
+        <label for="name"></label>
+        </form>
+    </div>
+@endsection
