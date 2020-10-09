30e974f8afe71a5a27d4d2189a4c01620c55fa08
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-10 01:42:44 +0900

    プロフィール作成アクションの追加

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $table = 'my_profiles';
-    protected $guarded = 'required';
+    protected $guarded = array('id');
 
+    public static $rules = array(
+        'name' => 'required',
+        'age' => 'integer|min:1|max:100',
+        'annualIncome' => '|min:0|max:2000',
+        'profession' => 'required'
+    );
 }
