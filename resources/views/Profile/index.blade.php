3a8e1701d79e54a799db73050ceb2f07f6a0838a
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-15 23:15:17 +0900

    キャッチコピーの修正

--- resources/views/Profile/index.blade.php
+++ resources/views/Profile/index.blade.php
@@ -6,8 +6,8 @@
 @section('content')
     <div class="main">
         <div class="main_container">
-            <h2 class="sub_copy">キャッチコピー1</h2>
-            <h1 class="main_copy">キャッチコピー２</h1>
+            <h2 class="sub_copy">SNSで繋がる新しい出会い</h2>
+            <h1 class="main_copy">SoulMate</h1>
             <a href="/login" class="sign in">ログイン</a>
             <a href="/register" class="sign up">今すぐ始める</a>
         </div>
