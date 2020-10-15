cd639b39ed427e55e0d2d584835a1889b14f4dda
澤田大樹 <hiroki.bigtree@icloud.com>
2020-10-15 23:47:52 +0900

    背景画像削除

--- resources/views/Profile/index.blade.php
+++ resources/views/Profile/index.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.header')
 @push('css')
-  <link rel="stylesheet" href="{{asset('/css/index.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/index.css')}}">
 @endpush
 
 @section('content')
     <div class="main">
         <div class="main_container">
-            <div style="background-image:url(lovers-2761553_1920.png);">
             <h2 class="sub_copy">SNSで繋がる新しい出会い</h2>
             <h1 class="main_copy">SoulMate</h1>
             <a href="/login" class="sign in">ログイン</a>
@@ -14,4 +13,3 @@
         </div>
     </div>
 @endsection
-    
