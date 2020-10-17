16124bcd6b302ee38eafec3ae0ce4087f3551ced
aocattleya <aocattleya@yahoo.co.jp>
2020-10-17 23:09:01 +0900

    フォーマット

--- resources/views/Profile/index.blade.php
+++ resources/views/Profile/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.header')
 @push('css')
-    <link rel="stylesheet" href="{{asset('/css/index.css')}}">
+<link rel="stylesheet" href="{{asset('/css/index.css')}}">
 @endpush
 
 @section('content')
-    <div class="main">
-        <div class="main_container">
-            <h2 class="sub_copy">SNSで見つける新しい出会い</h2>
-            <h1 class="main_copy">SoulMate</h1>
-            <a href="/login" class="sign in">ログイン</a>
-            <a href="/register" class="sign up">今すぐ始める</a>
-        </div>
+<div class="main">
+    <div class="main_container">
+        <h2 class="sub_copy">SNSで見つける新しい出会い</h2>
+        <h1 class="main_copy">SoulMate</h1>
+        <a href="/login" class="sign in">ログイン</a>
+        <a href="/register" class="sign up">今すぐ始める</a>
     </div>
+</div>
 @endsection
--- resources/views/Profile/like.blade.php
+++ resources/views/Profile/like.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>いいねー！！</title>
 </head>
+
 <body>
-<div>
-    @if($reply->is_liked_by_auth_user())
+    <div>
+        @if($reply->is_liked_by_auth_user())
         <a href="{{ route('reply.unlike', ['id' => $reply->id]) }}" class="btn btn-success btn-sm">いいね<span class="badge">{{ $reply->likes->count() }}</span></a>
-    @else
+        @else
         <a href="{{ route('reply.like', ['id' => $reply->id]) }}" class="btn btn-secondary btn-sm">いいね<span class="badge">{{ $reply->likes->count() }}</span></a>
-    @endif
-    {{ $reply->likes->count() }}
-</div>
+        @endif
+        {{ $reply->likes->count() }}
+    </div>
 </body>
+
 </html>
